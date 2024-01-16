79d5e676094d09950f3e944125f533c427f12e06
silkycode <silkycode@users.noreply.github.com>
2024-01-15 18:00:41 -0800

    Added footer and footer CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Footer from './Footer';
 
 function App() {
   return (
     <>
       <h1>AimPlus Medical Supplies</h1>
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (    
+        <footer>
+            <nav>
+                <a href="#">Help</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact</a>
+            </nav>
+        </footer>
+    );
+}
+
+export default Footer;
