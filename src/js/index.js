554a609ae60dc76035323ae16aec2f6aecafbde3
silkycode <silkycode@users.noreply.github.com>
2024-01-28 05:58:19 -0800

    Added some basic styling to landing home page, wrote placeholder dummy scripts for registration and login functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,21 @@
 function showLoginForm() {
-    document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('newPatientForm').style.display = 'block';
-    document.getElementById('login-container').style.display = 'block';
-    document.getElementById('new-patient-container').style.display = 'none';
-  }
+  document.getElementById("login-container").style.display = "block";
+  document.getElementById("new-patient-container").style.display = "none";
+  document.getElementById("form-buttons").getElementsByTagName("h2")[0].style.display = "none";
+}
 
-  function showNewPatientForm() {
-    document.getElementById('newPatientForm').style.display = 'none';
-    document.getElementById('loginForm').style.display = 'block';
-    document.getElementById('login-container').style.display = 'none';
-    document.getElementById('new-patient-container').style.display = 'block';
-  }
+function showNewPatientForm() {
+  document.getElementById("login-container").style.display = "none";
+  document.getElementById("new-patient-container").style.display = "block";
+  document.getElementById("form-buttons").getElementsByTagName("h2")[0].style.display = "none";
+}
+
+function dummyLogin() {
+  alert("Dummy login - logic to be implemented");
+  window.location.href = './src/pages/patient-dashboard.html'
+}
+
+function dummySubmitNewPatientForm() {
+  alert("Dummy submit new patient form - logic to be implemented" + "\n" + "Assume user is entered");
+  window.location.href = 'index.html';
+}
