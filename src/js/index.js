36bbdbc5a63cdcf67fb05757e8b284fdbf0d3927
silkycode <silkycode@users.noreply.github.com>
2024-01-31 04:53:26 -0800

    Added validation to login form

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,35 @@ function dummySubmitNewPatientForm() {
   alert("Dummy submit new patient form - logic to be implemented" + "\n" + "Assume user is entered");
   window.location.href = 'index.html';
 }
+
+
+// To add: greater validation for correct username/password format
+function validateLoginForm() {
+  var usernameInput = document.getElementById("login-username");
+  var passwordInput = document.getElementById("login-password");
+
+  var usernameError = document.getElementById("usernameError");
+  var passwordError = document.getElementById("passwordError");
+
+  usernameError.innerHTML = "";
+  passwordError.innerHTML = "";
+
+  validateUsername(usernameInput, usernameError);
+  validatePassword(passwordInput, passwordError);
+
+  if (usernameError.innerHTML === "" && passwordError.innerHTML === "") {
+    dummyLogin();
+  }
+}
+
+function validateUsername(usernameInput, usernameError) {
+  if (usernameInput.value.trim() === "") {
+    usernameError.innerHTML = "Username is required.";
+  }
+}
+
+function validatePassword(passwordInput, passwordError) {
+  if (passwordInput.value.trim() === "") {
+    passwordError.innerHTML = "Password is required.";
+  }
+}
